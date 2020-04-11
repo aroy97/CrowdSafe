d310ab46860e5e9f38534945bf81e9245a3f0029
SOUHARDYA ADHIKARY <souhardyaadhikary86942@gmail.com>
2020-04-11 21:45:58 +0530

    splash activity added

--- app/src/main/java/com/example/styledmap/SplashActivity.java
+++ app/src/main/java/com/example/styledmap/SplashActivity.java
@@ -1,6 +1,8 @@
 package com.example.styledmap;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.os.Handler;
 
 import androidx.appcompat.app.AppCompatActivity;
 
@@ -10,5 +12,22 @@ public class SplashActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_splash);
+        final int splash_display_time_length = 1500;
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+
+                if(splash_display_time_length == 500) {
+                    Intent intent = new Intent(SplashActivity.this, LoginActivity.class);
+                    SplashActivity.this.startActivity(intent);
+                    SplashActivity.this.finish();
+                } else {
+                    Intent intent = new Intent(SplashActivity.this, LoginActivity.class);
+                    SplashActivity.this.startActivity(intent);
+                    SplashActivity.this.finish();
+                }
+
+            }
+        }, splash_display_time_length);
     }
 }
