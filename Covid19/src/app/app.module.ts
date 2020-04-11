cb817ff88c5b15d0945fbae1de6b80772dc23463
Akash Roy <akash.roy.ece19@heritageit.edu.in>
2020-04-11 16:54:56 +0530

    Added fusion

--- Covid19/src/app/app.module.ts
+++ Covid19/src/app/app.module.ts
@@ -12,9 +12,16 @@ import { LoginComponent } from './login/login.component';
 import { RegisterComponent } from './register/register.component';
 import { MainpageComponent } from './mainpage/mainpage.component';
 import { NgxLoadingModule } from 'ngx-loading';
+import * as FusionCharts from 'fusioncharts';
+import { FusionChartsModule } from 'angular-fusioncharts';
+import * as FusionMaps from "fusionmaps/fusioncharts.maps";
+import * as FusionTheme from 'fusioncharts/themes/fusioncharts.theme.fusion';
+import * as India from 'fusioncharts/maps/fusioncharts.india';
 import { HeatmapComponent } from './heatmap/heatmap.component';
 
 
+
+FusionChartsModule.fcRoot(FusionCharts, FusionMaps, India, FusionTheme);
 
 @NgModule({
   declarations: [
