b3618064364357663c2f68df5dac39e2e0e7599b
SOUHARDYA ADHIKARY <souhardyaadhikary86942@gmail.com>
2020-04-11 23:15:35 +0530

    main activity created

--- app/src/main/java/com/example/styledmap/MainActivity.java
+++ app/src/main/java/com/example/styledmap/MainActivity.java
@@ -1,14 +1,33 @@
 package com.example.styledmap;
 
 import android.os.Bundle;
+import android.view.View;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import com.google.android.material.button.MaterialButton;
+
 public class MainActivity extends AppCompatActivity {
 
+    MaterialButton checkLocation,enterPicture;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        checkLocation=(MaterialButton)findViewById(R.id.checkLocation);
+        enterPicture=(MaterialButton)findViewById(R.id.enterPicture);
+
+        checkLocation.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+            }
+        });
+        enterPicture.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+            }
+        });
     }
 }
